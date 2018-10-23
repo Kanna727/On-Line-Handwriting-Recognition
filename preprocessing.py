08dbe7c7f8ac5c27e09fae175dc9fb2fd421db25
Dedsec727 <chitturiprasanth13797@gmail.com>
2018-10-23 23:13:07 +0530

    added more samples

--- preprocessing.py
+++ preprocessing.py
@@ -4,32 +4,40 @@ from FeatureExtractionCodes.features import features
 import csv
 import os
 
-filename = input('Enter the filepath:')
+def preProcessing(filename):
+    d = showpoints('Original Plot', filename)
+    d = interpolation(d)
+    final_features = features(d)
+    target = int(input("Enter stroke label(For only training):"))
+    final_features.insert(0, target)
+    print(final_features)
+    print(len(final_features))
 
-d = showpoints('Original Plot', filename)
-d = interpolation(d)
-final_features = features(d)
-target = int(input("Enter stroke label(For only training):"))
-final_features.insert(0, target)
-print(final_features)
-print(len(final_features))
+
+    title_row = ['Label']
+    for i in range(360):
+        title_row.append(i + 1)
+    filename = os.path.expanduser("~\\Desktop\\training_dataset.csv")
+    # print(filename)
+    if os.path.exists(filename):
+        # print('exists')
+        append_write = 'a' # append if already exists...
+    else:
+        # print('not exist')
+        append_write = 'w+' # make a new file if not
+
+    myFile = open(filename, append_write, newline='')
+    with myFile:
+        writer = csv.writer(myFile)
+        writer.writerow(final_features)
+    myFile.close()
+    # d = smoothing(d, 4, filename)
+
+path = input('Enter the folder path:')
+
+for f in os.listdir(path):
+    preProcessing(f)
 
 
-title_row = ['Label']
-for i in range(360):
-    title_row.append(i + 1)
-filename = os.path.expanduser("~\\Desktop\\training_dataset.csv")
-# print(filename)
-if os.path.exists(filename):
-    # print('exists')
-    append_write = 'a' # append if already exists...
-else:
-    # print('not exist')
-    append_write = 'w+' # make a new file if not
 
-myFile = open(filename, append_write, newline='')
-with myFile:
-    writer = csv.writer(myFile)
-    writer.writerow(final_features)
-myFile.close()
-# d = smoothing(d, 4, filename)
+
