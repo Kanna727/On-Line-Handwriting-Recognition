c3a7220ccd70cea85c8b695ba03cbfa689aa0743
Dedsec727 <chitturiprasanth13797@gmail.com>
2018-10-30 13:08:08 +0530

    done stroke segmentation

--- FeatureExtractionCodes/features.py
+++ FeatureExtractionCodes/features.py
@@ -9,136 +9,132 @@ import math
 
 
 def features(d):
-    strokes_list = []
-    l = []
+    # strokes_list = []
+    # l = []
 
-    for coordinate in d:
-        if coordinate[2] == 0.0:
-            l.append(coordinate)
-        else:
-            strokes_list.append(l)
-            l = []
+    # for coordinate in d:
+    #     if coordinate[2] == 0.0:
+    #         l.append(coordinate)
+    #     else:
+    #         strokes_list.append(l)
+    #         l = []
 
     zones = []
     stroke_wise_zones =[]
     zone_features = []
 
-    for stroke in strokes_list:
-        zone1 = []
-        zone2 = []
-        zone3 = []
-        zone4 = []
-        zone5 = []
-        zone6 = []
-        zone7 = []
-        zone8 = []
-        zone9 = []
-        zone_list = []
-
-        sz = len(stroke)  # no. of rows in d, i.e, no. of coordinates
-        x_coor = []
-        y_coor = []
-        for j in range(sz):  # this loop makes a list of all x coordinates
-            x_coor.append((stroke[j][0]))
-        max_x = max(x_coor)  # max and min values of x coordinate
-        min_x = min(x_coor)
-        for j in range(sz):  # this loop makes a list of all y coordinates
-            y_coor.append((stroke[j][1]))
-        max_y = max(y_coor)  # max and min values of y coordinate
-        min_y = min(y_coor)
-        #print(min_x, max_x, min_y, max_y)
-        dx = max_x - min_x  # Width of the grid
-        dy = max_y - min_y  # Height of the grid
-        l = dx / 3
-        m = 2 * (dx / 3)
-        a = dy / 3
-        b = 2 * (dy / 3)
-        #print(min_x, l + min_x, m + min_x, min_y, a + min_y, b + min_y)
-        for point in stroke:
-            if point[0] <= min_x + l:
-                if point[1] <= min_y + a:
-                    zone1.append(point)
-                if min_y + a < point[1] <= min_y + b:
-                    zone4.append(point)
-                if point[1] > min_y + b:
-                    zone7.append(point)
-            if min_x + l < point[0] <= min_x + m:
-                if point[1] <= min_y + a:
-                    zone2.append(point)
-                if min_y + a < point[1] <= min_y + b:
-                    zone5.append(point)
-                if point[1] > min_y + b:
-                    zone8.append(point)
-            if point[0] > min_x + m:
-                if point[1] <= min_y + a:
-                    zone3.append(point)
-                if min_y + a < point[1] <= min_y + b:
-                    zone6.append(point)
-                if point[1] > min_y + b:
-                    zone9.append(point)
-        zone_list.append(zone1)
-        zone_list.append(zone2)
-        zone_list.append(zone3)
-        zone_list.append(zone4)
-        zone_list.append(zone5)
-        zone_list.append(zone6)
-        zone_list.append(zone7)
-        zone_list.append(zone8)
-        zone_list.append(zone9)
-
-        # print([zone1, zone2, zone3, zone4, zone5, zone6, zone7, zone8, zone9])
-
-        zones = []
-        for zone in zone_list:
+    # fr stroke in strokes_list:
+    zone1 = []
+    zone2 = []
+    zone3 = []
+    zone4 = []
+    zone5 = []
+    zone6 = []
+    zone7 = []
+    zone8 = []
+    zone9 = []
+    zone_list = []
+    sz = len(d)  # no. of rows in d, i.e, no. of coordinates
+    x_coor = []
+    y_coor = []
+    for j in range(sz):  # this loop makes a list of all x coordinates
+        x_coor.append((d[j][0]))
+    max_x = max(x_coor)  # max and min values of x coordinate
+    min_x = min(x_coor)
+    for j in range(sz):  # this loop makes a list of all y coordinates
+        y_coor.append((d[j][1]))
+    max_y = max(y_coor)  # max and min values of y coordinate
+    min_y = min(y_coor)
+    #print(min_x, max_x, min_y, max_y)
+    dx = max_x - min_x  # Width of the grid
+    dy = max_y - min_y  # Height of the grid
+    l = dx / 3
+    m = 2 * (dx / 3)
+    a = dy / 3
+    b = 2 * (dy / 3)
+    #print(min_x, l + min_x, m + min_x, min_y, a + min_y, b + min_y)
+    for point in d:
+        if point[0] <= min_x + l:
+            if point[1] <= min_y + a:
+                zone1.append(point)
+            if min_y + a < point[1] <= min_y + b:
+                zone4.append(point)
+            if point[1] > min_y + b:
+                zone7.append(point)
+        if min_x + l < point[0] <= min_x + m:
+            if point[1] <= min_y + a:
+                zone2.append(point)
+            if min_y + a < point[1] <= min_y + b:
+                zone5.append(point)
+            if point[1] > min_y + b:
+                zone8.append(point)
+        if point[0] > min_x + m:
+            if point[1] <= min_y + a:
+                zone3.append(point)
+            if min_y + a < point[1] <= min_y + b:
+                zone6.append(point)
+            if point[1] > min_y + b:
+                zone9.append(point)
+    zone_list.append(zone1)
+    zone_list.append(zone2)
+    zone_list.append(zone3)
+    zone_list.append(zone4)
+    zone_list.append(zone5)
+    zone_list.append(zone6)
+    zone_list.append(zone7)
+    zone_list.append(zone8)
+    zone_list.append(zone9)
+    # print([zone1, zone2, zone3, zone4, zone5, zone6, zone7, zone8, zone9])
+    zones = []
+    for zone in zone_list:
+        vicinity = []
+        bin_curl = [0 for x in range(8)]
+        bin_lin = [0 for x in range(8)]
+        bin_sl = [0 for x in range(8)]
+        bin_dir = [0 for x in range(8)]
+        bin_curv = [0 for x in range(8)]
+        if len(zone) != 0:
+            for i in range(0, len(zone), 11):
+                j = i
+                while j < (i + 11) and j < len(zone):
+                    vicinity.append(zone[j])
+                    j = j + 1
+                bin = quantizer(math.acos(curliness(vicinity)))
+                bin_curl[bin] = bin_curl[bin] + 1
+                bin = quantizer(math.atan(linearity(vicinity)))
+                bin_lin[bin] += 1
+                bin = quantizer(slope(vicinity))
+                bin_sl[bin] += 1
+                vicinity = []
             vicinity = []
-            bin_curl = [0 for x in range(8)]
-            bin_lin = [0 for x in range(8)]
-            bin_sl = [0 for x in range(8)]
-            bin_dir = [0 for x in range(8)]
-            bin_curv = [0 for x in range(8)]
-
-            if len(zone) != 0:
-                for i in range(0, len(zone), 11):
-                    j = i
-                    while j < (i + 11) and j < len(zone):
-                        vicinity.append(zone[j])
-                        j = j + 1
-                    bin = quantizer(math.acos(curliness(vicinity)))
-                    bin_curl[bin] = bin_curl[bin] + 1
-                    bin = quantizer(math.atan(linearity(vicinity)))
-                    bin_lin[bin] += 1
-                    bin = quantizer(slope(vicinity))
-                    bin_sl[bin] += 1
-                    vicinity = []
+            for i in range(0, len(zone), 3):
+                j = i
+                while j < (i + 3) and j < len(zone):
+                    vicinity.append(zone[j])
+                    j = j + 1
+                bin = quantizer(direction(vicinity))
+                bin_dir[bin] += 1
                 vicinity = []
-                for i in range(0, len(zone), 3):
-                    j = i
-                    while j < (i + 3) and j < len(zone):
-                        vicinity.append(zone[j])
-                        j = j + 1
-                    bin = quantizer(direction(vicinity))
-                    bin_dir[bin] += 1
-                    vicinity = []
+            vicinity = []
+            for i in range(0, len(zone), 5):
+                j = i
+                while j < (i + 5) and j < len(zone):
+                    vicinity.append(zone[j])
+                    j = j + 1
+                bin = quantizer(curvature(vicinity))
+                bin_curv[bin] += 1
                 vicinity = []
-                for i in range(0, len(zone), 5):
-                    j = i
-                    while j < (i + 5) and j < len(zone):
-                        vicinity.append(zone[j])
-                        j = j + 1
-                    bin = quantizer(curvature(vicinity))
-                    bin_curv[bin] += 1
-                    vicinity = []
-                for i in range(8):
-                    bin_curv[i] /= len(zone)
-                    bin_dir[i] /= len(zone)
-                    bin_sl[i] /= len(zone)
-                    bin_lin[i] /= len(zone)
-                    bin_curl[i] /= len(zone)
-            zone_features.extend(bin_dir)
-            zone_features.extend(bin_curv)
-            zone_features.extend(bin_sl)
-            zone_features.extend(bin_lin)
-            zone_features.extend(bin_curl)
+            for i in range(8):
+                bin_curv[i] /= len(zone)
+                bin_dir[i] /= len(zone)
+                bin_sl[i] /= len(zone)
+                bin_lin[i] /= len(zone)
+                bin_curl[i] /= len(zone)
+        zone_features.extend(bin_dir)
+        zone_features.extend(bin_curv)
+        zone_features.extend(bin_sl)
+        zone_features.extend(bin_lin)
+        zone_features.extend(bin_curl)
     return zone_features
 
 
--- train_code.py
+++ train_code.py
@@ -4,44 +4,41 @@ import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn import preprocessing
 from sklearn import utils
+from test_code import predictStrokeLabel
 from PreprocessingCodes.showpoints import showpoints
 from PreprocessingCodes.interpolation import interpolation
 from FeatureExtractionCodes.features import features
 
 def getFeatures(folderPath, filename):
     d = showpoints('Original Plot', filename, folderPath)
+    print(d)
     d = interpolation(d)
     final_features = features(d)
     return final_features
 
+### Training Part Starts ### 
 x=pd.read_csv("training_dataset.csv")
-#b = x[["Class"]].copy()
 c = np.array(x)
 y = c[:,0]
-#y = np.array(b)
 x.drop(["Class"], axis=1,inplace=True)
 a=np.array(x)
 
-#p = pd.DataFrame(x).set_index('id')[10].copy(deep=True)
-
-
-#print(y)
-
 
 lab_enc = preprocessing.LabelEncoder()
-#encoded = lab_enc.fit_transform(y)
-#encoded = lab_enc.fit_transform(x)
 
-#clf = SVC(kernel='rbf') 
 clf = SVC(C=1.0, kernel='poly',degree=3, gamma=2)
 clf.fit(x, y) 
+### Training Part Ends ###
 
-# folderPath = input("Enter folder path: ")
-# fileName = input("Enter file name: ")
+### Testing Part Starts ###
+folderPath = input("Enter folder path: ")
+fileName = input("Enter file name: ")
 
-# features = getFeatures(folderPath, fileName)
+features = getFeatures(folderPath, fileName)
+result = clf.predict([features])
+print(int(result[0]))
+### Testing Part Ends ###
 
-# print(clf.predict([features]))
 print(clf.score(x,y))
 
 
