ae0ee606aa58593e62f98ca0ff03e3deb6623079
Dedsec727 <33746906+Kanna727@users.noreply.github.com>
2019-05-23 23:33:42 +0530

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -5,18 +5,8 @@ from sklearn.svm import SVC
 from sklearn import preprocessing
 from sklearn import utils
 
-def predictStrokeLabel(featuresList):
-    x=pd.read_csv("training_dataset.csv")
-    c = np.array(x)
-    y = c[:,0]
-    x.drop(["Class"], axis=1,inplace=True)
-    a=np.array(x)
-
-
-    lab_enc = preprocessing.LabelEncoder()
-
-    clf = SVC(C=1.0, kernel='poly',degree=3, gamma=2)
-    clf.fit(x, y) 
+def predictStrokeLabel(featuresList,clf):
+    
 
     result = clf.predict([featuresList])
 
