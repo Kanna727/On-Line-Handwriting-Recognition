23b2293a8d907e919450ddd766afc10ee6fa5d52
Dedsec727 <33746906+Kanna727@users.noreply.github.com>
2019-05-24 00:28:32 +0530

    Update stroke_segmentation.py

--- stroke_segmentation.py
+++ stroke_segmentation.py
@@ -2,22 +2,36 @@ from PreprocessingCodes.interpolation import interpolation
 from FeatureExtractionCodes.features import features
 #from test_code import predictStrokeLabel
 import matplotlib.pyplot as plt
-import numpy as np 
-import pandas as pd 
-import matplotlib.pyplot as plt 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn import preprocessing
 from sklearn import utils
+
+
+
 def strokeSeg(content):
     strokesList = []
     tempStrokeList=[]
     strokeLabelsList = []
-    def predictStrokeLabel(featuresList):
-  
+    x = pd.read_csv("training_dataset.csv")
+    print(np.isnan(x))
 
-       result = clf.predict([featuresList])
+    c = np.array(x)
+    y = c[:, 0]
+    x.drop(["Class"], axis=1, inplace=True)
+    a = np.array(x)
 
-       return int(result[0])
+    lab_enc = preprocessing.LabelEncoder()
+    clf = SVC(C=1.0, kernel='poly', degree=3, gamma=2)
+    clf.fit(x, y)
+
+    def predictStrokeLabel(featuresList,clf):
+
+        result = clf.predict([featuresList])
+
+        return int(result[0])
     # folderPath = 'C:\\Users\\DEDSEC\\OneDrive\\Documents\\VSCode\\Minor Project\\On-Line-Handwriting-Recognition\\Letter Test Samples\\Working'
 
     # fileName = input('Enter the file name:')
@@ -25,18 +39,6 @@ def strokeSeg(content):
 
     # with open(path) as f:
     #     content = f.readlines()
-    x=pd.read_csv("training_dataset.csv")
-    print(np.isnan(x))
-
-    c = np.array(x)
-    y = c[:,0]
-    x.drop(["Class"], axis=1,inplace=True)
-    a=np.array(x)
-
-    lab_enc = preprocessing.LabelEncoder()
-    clf = SVC(C=1.0, kernel='poly',degree=3, gamma=2)
-    clf.fit(x, y)     
-
 
     content = [z.strip().split(',') for z in content]
     for item in content:
@@ -78,7 +80,7 @@ def strokeSeg(content):
 
 # compareLabel(strokeLabelsList)
 # return strokeLabelsList
-
+    print(letter)
     return letter
     
 
